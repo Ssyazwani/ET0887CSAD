f2bcf7e5061aa418d3efd83f25b4b4140821763c
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-10-24 10:40:58 +0800

    testing out javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+//Setting values and printing:
+
+
+function MyFunction(){
+// Variable Declarations
+let num = 45;
+let x, y, sum;
+let Name = "Apple";
+
+// Assign value to the variables
+x = 3;
+y = 23;
+
+// Use arithmetic operator to
+// add two numbers
+sum = x + y;
+
+console.log(Name) //to print
+
+console.log(num) // print local value
+
+console.log(sum)
+
+}
+
+MyFunction();// calling to use function
