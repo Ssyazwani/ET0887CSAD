fd65caacb1fdf6d9dd9bab7421304fc31b0a0acd
Ssyazwani <148629147+Ssyazwani@users.noreply.github.com>
2025-11-23 19:41:45 +0800

    doing Q5 and Q4, explaining Q6

--- labtestex/Q5.js
+++ labtestex/Q5.js
@@ -1,7 +1,28 @@
 const A = [88, 73, 59, 61];
 const B = [80, 0, -66, 97, -44];
 
-// write your function CombineArray here
+function CombineArray(A, B) {
+ 
+    const newArray = [...A];
+
+ 
+    for (let value of B) {
+        if (value < 0) {
+         
+            value = 100 - Math.abs(value);
+        }
+        newArray.push(value);
+    }
+
+
+
+    return newArray;
+}
+
 
 console.log(CombineArray(A, B));
 
+
+    for (let value of B) { //print out array
+    console.log(value);
+}
