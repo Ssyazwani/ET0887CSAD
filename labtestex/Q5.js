b90bc1e2ce5f9649f732ad566e29400853bd71b4
Ssyazwani <148629147+Ssyazwani@users.noreply.github.com>
2025-11-24 20:06:38 +0800

    editing labtestexample

--- labtestex/Q5.js
+++ labtestex/Q5.js
@@ -1,28 +1,44 @@
 const A = [88, 73, 59, 61];
 const B = [80, 0, -66, 97, -44];
 
-function CombineArray(A, B) {
+// function CombineArray(A, B) {
  
-    const newArray = [...A];
+//     const newArray = [...A];
 
  
-    for (let value of B) {
-        if (value < 0) {
-         
-            value = 100 - Math.abs(value);
+//     for (let value of B) {
+//         if (value < 0) {
+//             value = 100 - Math.abs(value);
+//         }
+//         newArray.push(value);
+//     }
+
+
+
+//     return newArray;
+// }
+
+
+
+   function CombineArray(x,y){
+    let z = [];
+    for (var i =0;i<x.length;i++){
+        if (x[i] <0){
+            z.push(100 + x[i]);
+        } else{
+            z.push(x[i]);
         }
-        newArray.push(value);
     }
 
-
-
-    return newArray;
+    for (var i =0;i<y.length;i++){
+         if (y[i] <0){
+            z.push(100 + y[i]);
+        } else{
+            z.push(y[i]);
+        }
+    }
+    return z;
 }
 
 
 console.log(CombineArray(A, B));
-
-
-    for (let value of B) { //print out array
-    console.log(value);
-}
